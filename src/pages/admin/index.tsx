35be50f8b2b6d3e0bd80b706c86d9d79964aa457
Sofiane Boumedine <sboumedine@LM-00202.local>
2023-04-24 09:43:30 +0200

    Add features for create articles from admin pages

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,5 +1,15 @@
+import styles from '../../styles/Admin.module.css';
 import AuthCheck from "@/components/AuthCheck";
+import PostFeed from "@/components/PostFeed";
+import { UserContext } from "@/lib/context";
+import { firestore, auth } from "@/lib/firebase";
+import { serverTimestamp } from "firebase/firestore";
+import { kebabCase } from "lodash";
 import Head from "next/head";
+import { useRouter } from "next/router";
+import { useContext, useState } from "react";
+import { useCollection } from "react-firebase-hooks/firestore";
+import toast from "react-hot-toast";
 
 export default function AdminPostsPage({ }) {
   return (
@@ -11,7 +21,71 @@ export default function AdminPostsPage({ }) {
           <meta name="twitter:site" content="@SB_dev" />
         </Head>
         <h1>Admin posts</h1>
+        <PostList />
+        <CreateNewPost />
       </AuthCheck>
     </main>
+  )
+}
+
+function PostList(){
+  const ref = firestore.collection('users').doc(auth.currentUser.uid).collection('posts');
+  const query = ref.orderBy('createdAt');
+  const [querySnapshot] = useCollection(query);
+
+  const posts = querySnapshot?.docs.map((doc)=> doc.data());
+
+  return (
+    <>
+      <h1>Gérer vos posts</h1>
+      <PostFeed posts={posts} admin />
+    </>
+  );
+}
+
+function CreateNewPost() {
+  const router = useRouter();
+  const {username} = useContext(UserContext);
+  const [title, setTitle] = useState('');
+
+  const slug = encodeURI(kebabCase(title));
+  const isValid = title.length > 3 && title.length < 100;
+
+  const createPost = async (e) => {
+    e.preventDefault();
+    const uid = auth.currentUser.uid;
+    const ref = firestore.collection('users').doc(uid).collection('posts').doc(slug);
+
+    const data = {
+      title,
+      slug,
+      uid,
+      username,
+      published: false,
+      content: '# hello world!',
+      createdAt: serverTimestamp(),
+      updatedAt: serverTimestamp(),
+      heartCount: 0,
+    };
+
+    await ref.set(data);
+    toast.success('Post created!');
+
+    router.push(`/admin/${slug}`);
+  };
+
+  return (
+    <form onSubmit={createPost}>
+      <input
+      value={title}
+      onChange={(e)=> setTitle(e.target.value)}
+      placeholder="My Awesome Article!"
+      className={styles.input}
+      />
+      <p><strong>Slug:</strong> {slug}</p>
+      <button type="submit" disabled={!isValid} className="btn-green">
+        Crée un nouveau Post
+      </button>
+    </form>
   )
 }
